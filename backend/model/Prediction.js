e47ed008ed59acb7bdd0868fe04da708380d19bd
sri bharathi <sribharathil_bit27@mepcoeng.ac.in>
2026-05-02 01:46:31 +0530

    add Prediction model

--- backend/model/Prediction.js
+++ backend/model/Prediction.js
@@ -1,24 +1,21 @@
 const mongoose = require("mongoose");
 
-const predictionSchema = new mongoose.Schema({
-    input: {
-        f1: Number,
-        f2: Number,
-        f3: Number,
-        f4: Number,
-        f5: Number
-    },
-    results: [
-        {
-            model: String,
-            value: Number,
-            deltaR: Number
-        }
-    ],
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+const PredictionSchema = new mongoose.Schema({
+  input: {
+    f1: Number,
+    f2: Number,
+    f3: Number,
+    f4: Number,
+    f5: Number
+  },
+  output: {
+    XGB:          Number,
+    ASE:          Number,
+    ML:           Number,
+    LWR:          Number,
+    FINAL_HYBRID: Number
+  },
+  createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Prediction", predictionSchema);
+module.exports = mongoose.model("Prediction", PredictionSchema);
